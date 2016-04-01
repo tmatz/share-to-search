00ab76f06a3221d715024e119ad56f606e39d870
MATSUDA Takashi <t.matsuda.1000@gmail.com>
2016-04-01 18:46:35 +0900

    icon. values-ja.

---
+++ app/src/main/java/io/github/tmatz/ShareToSearch/SendTextActivity.java
@@ -0,0 +1,28 @@
+package io.github.tmatz.ShareToSearch;
+
+import android.app.*;
+import android.os.*;
+import android.content.*;
+
+public class SendTextActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		if (Intent.ACTION_SEND.equals(action)) {
+			Bundle extras = intent.getExtras();
+			if (extras != null) {
+				CharSequence ext = extras.getCharSequence(Intent.EXTRA_TEXT);
+				if (ext != null) {
+					Intent intentSearch = new Intent(Intent.ACTION_WEB_SEARCH);
+					intentSearch.putExtra(SearchManager.QUERY, ext);
+					startActivity(intentSearch);
+				}
+			}
+		}
+		finish();
+    }
+}
