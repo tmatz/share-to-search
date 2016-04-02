92dff7b7fe393ea98619c83df575b30f825182f3
MATSUDA Takashi <t.matsuda.1000@gmail.com>
2016-04-02 11:51:41 +0900

    clean code.

--- app/src/main/java/io/github/tmatz/ShareToSearch/MainActivity.java
+++ app/src/main/java/io/github/tmatz/ShareToSearch/MainActivity.java
@@ -10,21 +10,6 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-		Intent intent = getIntent();
-		String action = intent.getAction();
-		if (Intent.ACTION_SEND.equals(action)) {
-			Bundle extras = intent.getExtras();
-			if (extras != null) {
-				CharSequence ext = extras.getCharSequence(Intent.EXTRA_TEXT);
-				if (ext != null) {
-					Intent intentSearch = new Intent(Intent.ACTION_WEB_SEARCH);
-					intentSearch.putExtra(SearchManager.QUERY, ext);
-					startActivity(intentSearch);
-					finish();
-					return;
-				}
-			}
-		}
         setContentView(R.layout.main);
     }
 }
